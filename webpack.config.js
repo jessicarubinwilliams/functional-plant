ec99360c70770b7060242939d91b05f445d68c1e
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-12-19 14:16:53 -0800

    Update configuration
    Co-Authored-by: Mary Marks <maryleemarks@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Document',
+      title: 'functional-plant',
       template: './src/index.html',
       inject: 'body'
     })
