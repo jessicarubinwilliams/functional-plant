2f66a14d54b0bda3e5b907ada0e7fac7046d270f
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-12-19 14:19:02 -0800

    Added fn to store state, fn factory & its output fns
    Co-Authored-by: Mary Marks <maryleemarks@gmail.com>

---
+++ src/js/plant.js
@@ -0,0 +1,31 @@
+// This function stores our state.
+
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+export const stateControl = storeState();
+
+// This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
+
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  }
+}
+
+// We create four functions using our function factory. We could easily create many more.
+
+// const feed = changeState("soil")(1);
+export const blueFood = changeState("soil")(5);
+
+// const hydrate = changeState("water")(1);
+// const superWater = changeState("water")(5);
