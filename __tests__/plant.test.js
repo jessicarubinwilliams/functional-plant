b2e10e7b29f9a8c8271804319b39ff7e3c563152
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-12-20 19:12:33 -0800

    Pass test for changeState()
    Co-Authored-by: Mary Marks <maryleemarks@gmail.com>

--- __tests__/plant.test.js
+++ __tests__/plant.test.js
@@ -1,9 +1,18 @@
-import { storeState, blueFood, feed, hydrate, superWater, plant } from './../src/js/plant.js';
+import { changeState, storeState, blueFood, feed, hydrate, superWater, plant } from './../src/js/plant.js';
 
 describe('storeState', () => {
 
   test('should correctly store the initial state', () => {
       const stateControl = storeState();
       expect(stateControl()).toEqual({});
+  });
+});
+
+describe('changeState', () => {
+
+  test('should correctly create functions that alter a plants properties', () => {
+    let testPlant = {}
+    const testFunction = changeState("testProperty")(2);
+    expect(testFunction(testPlant)).toEqual({ testProperty: 2 });
   });
 });
